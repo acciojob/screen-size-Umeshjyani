67870161274628cb817e1bc4ecd560a424c798a6
umesh jyani <49265224+Umeshjyani@users.noreply.github.com>
2023-08-10 19:44:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let x = document.getElementById("show");
+function showhw() {
+	let w = window.innerWidth;
+	let h = window.innerHeight;
+	x.innerHTML = "Width: " + w + " and Height: " + h;
+}
+showhw();
+window.addEventListener('resize', showhw);
